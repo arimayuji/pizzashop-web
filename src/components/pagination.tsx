b2dc34f66241175ff85daaa62b1da2f8096be945
Yuji Arima <arimayuji2@gmail.com>
2024-02-26 10:11:38 -0300

    components: custom pagination

---
+++ src/components/pagination.tsx
@@ -0,0 +1,46 @@
+import { ChevronRight, ChevronsLeft, ChevronsRight } from 'lucide-react'
+
+import { Button } from './ui/button'
+
+export interface PaginationProps {
+  pageIndex: number
+  perPage: number
+  totalCount: number
+}
+export function Pagination({
+  pageIndex,
+  perPage,
+  totalCount,
+}: PaginationProps) {
+  const pages = Math.ceil(totalCount / perPage) || 1
+  return (
+    <div className="flex items-center justify-between">
+      <span className="text-sm text-muted-foreground">
+        Total de {totalCount} item(s)
+      </span>
+      <div className="flex items-center gap-6 lg:gap-8">
+        <div className="text-sm font-medium">
+          Página {pageIndex + 1} de {pages}
+        </div>
+        <div className="flex items-center gap-2">
+          <Button variant="outline">
+            <ChevronsLeft className="h-4 w-4" />
+            <span className="sr-only">Primeira Página</span>
+          </Button>
+          <Button variant="outline">
+            <ChevronsLeft className="h-4 w-4" />
+            <span className="sr-only">Página Anterior</span>
+          </Button>
+          <Button variant="outline">
+            <ChevronRight className="h-4 w-4" />
+            <span className="sr-only">Próxima Página</span>
+          </Button>
+          <Button variant="outline">
+            <ChevronsRight className="h-4 w-4" />
+            <span className="sr-only">Última Página</span>
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
