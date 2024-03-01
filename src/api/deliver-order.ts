bb5dbe91a707b334f98d9763b0a661dc275b3ac9
Yuji Arima <arimayuji2@gmail.com>
2024-03-01 19:44:05 -0300

    api - patch status order

---
+++ src/api/deliver-order.ts
@@ -0,0 +1,8 @@
+import { api } from '@/lib/axios'
+
+interface DeliverOrderParams {
+  orderId: string
+}
+export async function deliverOrder({ orderId }: DeliverOrderParams) {
+  await api.patch(`/orders/${orderId}/deliver`)
+}
