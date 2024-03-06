ee21c278769c8ea4f4b88a7c184a86910b8cca33
Yuji Arima <arimayuji2@gmail.com>
2024-03-06 10:00:58 -0300

    test: :white_check_mark: mocking for cancel order action

--- src/api/cancel-order.ts
+++ src/api/cancel-order.ts
@@ -1,6 +1,6 @@
 import { api } from '@/lib/axios'
 
-interface CancelOrderParams {
+export interface CancelOrderParams {
   orderId: string
 }
 export async function cancelOrder({ orderId }: CancelOrderParams) {
